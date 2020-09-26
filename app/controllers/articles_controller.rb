34dd9d06f4f1d2d3789f37b02a7a983b8bd0c898
8bitbran <brandonmar97@gmail.com>
2020-09-25 22:17:21 -0400

    Updated create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,14 @@ class ArticlesController < ApplicationController
     end 
 
     def create
-        render plain: params[:article]
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to @article
     end     
+
+    private 
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 end 
